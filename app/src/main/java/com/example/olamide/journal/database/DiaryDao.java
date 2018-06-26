5f4d27a6ea3714f4b314bf1055b57e7a616a7e44
Olamide Laleye <laleyeolamide@yahoo.com>
2018-06-26 14:15:52 +0100

    room init

---
+++ app/src/main/java/com/example/olamide/journal/database/DiaryDao.java
@@ -0,0 +1,29 @@
+package com.example.olamide.journal.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+public interface DiaryDao {
+
+    @Query("SELECT * FROM diary ORDER BY created_at")
+    LiveData<List<DiaryEntry>> loadAllEntries();
+
+    @Insert
+    void insertEntry(DiaryEntry diaryEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateEntry(DiaryEntry diaryEntry);
+
+    @Delete
+    void deleteEntry(DiaryEntry diaryEntry);
+
+    @Query("SELECT * FROM diary WHERE id = :id")
+    LiveData<DiaryEntry> loadEntryById(int id);
+
+}
---
+++ app/src/main/java/com/example/olamide/journal/database/DiaryEntry.java
@@ -0,0 +1,60 @@
+package com.example.olamide.journal.database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "diary")
+public class DiaryEntry {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String mood;
+    private String diaryContent;
+    @ColumnInfo(name = "created_at")
+    private Date createdAt;
+    @ColumnInfo(name = "updated_at")
+    private Date updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getDiaryContent() {
+        return diaryContent;
+    }
+
+    public void setDiaryContent(String diaryContent) {
+        this.diaryContent = diaryContent;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
