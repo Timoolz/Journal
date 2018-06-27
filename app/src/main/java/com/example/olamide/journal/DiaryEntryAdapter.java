e36b8b72a880bf3ea9fb70c968412f296337cf93
Olamide Laleye <laleyeolamide@yahoo.com>
2018-06-27 13:06:07 +0100

    Recycler View and adapter

--- app/src/main/java/com/example/olamide/journal/DiaryEntryAdapter.java
+++ app/src/main/java/com/example/olamide/journal/DiaryEntryAdapter.java
@@ -1,4 +1,127 @@
 package com.example.olamide.journal;
 
-public class DiaryEntryAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.olamide.journal.database.DiaryEntry;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class DiaryEntryAdapter extends RecyclerView.Adapter<DiaryEntryAdapter.DiaryEntryViewHolder> {
+
+
+    private static final String DATE_FORMAT = "dd/MM/yyyy";
+
+    // Member variable to handle item clicks
+    final private ItemClickListener mItemClickListener;
+
+    private List<DiaryEntry> mDiaryEntries;
+    private Context mContext;
+    // Date formatter
+    private SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+
+
+    public DiaryEntryAdapter(Context context, ItemClickListener listener) {
+        mContext = context;
+        mItemClickListener = listener;
+    }
+
+
+    @Override
+    public DiaryEntryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflate the task_layout to a view
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.diary_entry_layout, parent, false);
+
+        return new DiaryEntryViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(DiaryEntryViewHolder holder, int position) {
+        // Determine the values of the wanted data
+        DiaryEntry diaryEntry = mDiaryEntries.get(position);
+
+        String mood = diaryEntry.getMood();
+        String diaryContent = diaryEntry.getDiaryContent();
+
+        String[] lines = diaryContent.split(System.getProperty("line.separator"));
+        String createdAt = dateFormat.format(diaryEntry.getCreatedAt());
+        String updatedAt = dateFormat.format(diaryEntry.getUpdatedAt());
+
+        //Set values
+        holder.moodView.setText(mood);
+        holder.diaryContentView.setText(lines[0]+ '\n' +lines[1] + '\n' +lines[2]+ '\n' +lines[3] +"...");
+
+        holder.datesView.setText("Modified on " +updatedAt+ "  /  Created on " +createdAt);
+    }
+
+
+
+    /**
+     * Returns the number of items to display.
+     */
+    @Override
+    public int getItemCount() {
+        if (mDiaryEntries == null) {
+            return 0;
+        }
+        return mDiaryEntries.size();
+    }
+
+
+    public List<DiaryEntry> getDiaryEntries() {
+        return mDiaryEntries;
+    }
+
+    /**
+     * When data changes, this method updates the list of diaryEntries
+     * and notifies the adapter to use the new values on it
+     */
+    public void setDiaryEntries(List<DiaryEntry> diaryEntries) {
+        mDiaryEntries = diaryEntries;
+        notifyDataSetChanged();
+    }
+
+    public interface ItemClickListener {
+        void onItemClickListener(int itemId);
+    }
+
+
+    // Inner class for creating ViewHolders
+    class DiaryEntryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        // Class variables for the task description and priority TextViews
+        TextView diaryContentView;
+        TextView moodView;
+        TextView datesView;
+
+        /**
+         * Constructor for the TaskViewHolders.
+         *
+         * @param itemView The view inflated in onCreateViewHolder
+         */
+        public DiaryEntryViewHolder(View itemView) {
+            super(itemView);
+
+            diaryContentView = itemView.findViewById(R.id.diaryContent);
+            moodView = itemView.findViewById(R.id.moodTextView);
+            datesView = itemView.findViewById(R.id.diaryDates);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int elementId = mDiaryEntries.get(getAdapterPosition()).getId();
+            mItemClickListener.onItemClickListener(elementId);
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/olamide/journal/MainActivity.java
+++ app/src/main/java/com/example/olamide/journal/MainActivity.java
@@ -1,13 +1,122 @@
 package com.example.olamide.journal;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.olamide.journal.database.DiaryEntry;
+import com.example.olamide.journal.database.JournalDatabase;
+
+import java.util.List;
+
+import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
+
+public class MainActivity extends AppCompatActivity implements DiaryEntryAdapter.ItemClickListener {
+
+
+
+    // Constant for logging
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // Member variables for the adapter and RecyclerView
+    private RecyclerView mRecyclerView;
+    private DiaryEntryAdapter mAdapter;
+
+    private JournalDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the RecyclerView to its corresponding view
+        mRecyclerView = findViewById(R.id.recyclerViewDiary);
+
+        // Set the layout for the RecyclerView to be a linear layout, which measures and
+        // positions items within a RecyclerView into a linear list
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the RecyclerView
+        mAdapter = new DiaryEntryAdapter(this, this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
+        mRecyclerView.addItemDecoration(decoration);
+
+
+
+        /*
+         Add a touch helper to the RecyclerView
+         */
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            // Called when a user swipes left or right on a ViewHolder
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                // Here is where you'll implement swipe to delete
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int position = viewHolder.getAdapterPosition();
+                        List<DiaryEntry> tasks = mAdapter.getDiaryEntries();
+                        mDb.diaryDao().deleteEntry(tasks.get(position));
+                    }
+                });
+            }
+        }).attachToRecyclerView(mRecyclerView);
+
+
+        FloatingActionButton fabButton = findViewById(R.id.fab);
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to start an AddTaskActivity
+                Intent addTaskIntent = new Intent(MainActivity.this, EditDiaryActivity.class);
+                startActivity(addTaskIntent);
+            }
+        });
+
+        mDb = JournalDatabase.getInstance(getApplicationContext());
+        setupViewModel();
+
     }
+
+    private void setupViewModel() {
+        MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        viewModel.getEntries().observe(this, new Observer<List<DiaryEntry>>() {
+            @Override
+            public void onChanged(@Nullable List<DiaryEntry> diaryEntries) {
+                Log.d(TAG, "Updating list of tasks from LiveData in ViewModel");
+                mAdapter.setDiaryEntries(diaryEntries);
+            }
+        });
+    }
+
+
+    @Override
+    public void onItemClickListener(int itemId) {
+        // Launch AddTaskActivity adding the itemId as an extra in the intent
+        Intent intent = new Intent(MainActivity.this, EditDiaryActivity.class);
+        intent.putExtra(EditDiaryActivity.EXTRA_DIARY_ENTRY_ID, itemId);
+        startActivity(intent);
+    }
+
+
+
+
+
 }
